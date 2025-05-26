3828d40654753638c17c7ec950102a8d34dad565
el-qafouri <elham.qafouri.91@gmail.com>
2025-05-26 12:46:15 +0430

    create and fix book class

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class Book {
+
+    private int id;
+    private String name;
+    private String author;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(String name, String author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(int id, String name, String author, double price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
